a319cd2640c4d9c319b485fea7b19053d15e3194
Suzan Al Majdalawi <suzan.al-majdalawi@chasacademy.se>
2026-08-31 00:48:49 +0200

    fix/uppgift_1

---
+++ src/utils/user.test.js
@@ -0,0 +1,8 @@
+import { it, expect } from 'vitest'
+import { firstName } from './user'
+
+it('returns the name when there is only one word', () => {
+  const result = firstName('Anna')
+
+  expect(result).toBe('Anna')
+})
