677d3ca4fadc126036d7830a2baac6d5df682bb8
Sai24020 <suzan.al-majdalawi@chasacademy.se>
2026-09-03 00:47:33 +0200

    chore: add Cypress for end-to-end testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ export default [
   {
     ignores: ["node_modules/**", "dist/**", "testresultat/**", "mock-api/**"],
   },
+
+  // Vanliga JS-filer
   {
     files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
     languageOptions: {
@@ -15,6 +17,32 @@ export default [
       "no-unused-vars": "warn",
       "no-undef": "error",
       "no-console": "off",
+    },
+  },
+
+
+  // Cypress
+  {
+    files: ["cypress/**/*.cy.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+
+        // Cypress
+        cy: "readonly",
+        Cypress: "readonly",
+
+        // Mocha / Cypress test functions
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        before: "readonly",
+        after: "readonly",
+      },
+    },
+    rules: {
+      "no-undef": "error",
     },
   },
 ];
